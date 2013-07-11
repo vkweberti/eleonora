c1f12357e227b81c99e12d3bd3ee2e7706051d00
vkweberti <vkweberti@gmail.com>
2013-07-11 07:29:43 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eleonora::Application.config.secret_token = '1b16332f91d1b4e7421009e3f9a98b03c65f77e7755faab085bc6bf61d715724b92f07337463465f4bcffe6e3b610e6a3db45fd1f96a82ccfa0d2f4e6fbc2740'
