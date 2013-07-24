5d80356eca18f2aca7dde1462ae8bfa19d87db56
vladimir-kurguz <kurguz.vladimir@rezonans.ru>
2013-07-24 13:37:21 +0300

    Product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ActiveRecord::Base
 
   attr_accessible :title, :description, :price, :size, :care, :is_new, :make, :material
 
+  validates :title, presence: true
+  validates :price, presence: true, :numericality => true
+  validates :size, presence: true, :numericality => true
+
 end
