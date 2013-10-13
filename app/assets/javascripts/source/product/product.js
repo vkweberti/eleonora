b255182237fb1946225866eecb79739252d0d491
vkweberti <vkweberti@gmail.com>
2013-10-13 23:10:48 +0300

    Product form

--- app/assets/javascripts/source/product/product.js
+++ app/assets/javascripts/source/product/product.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   //вешаем плагин на контейнер-картинку
   $(".product .foto").imagezoomsl({
-    zoomrange: [2, 12],
+    zoomrange: [2.5, 12],
     magnifiersize: [400, 450],
     scrollspeedanimate: 10,
     loopspeedanimate: 5
