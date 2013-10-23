3343d248e097fb11f95d5a6921b16bf5dcc950cd
vladimir-kurguz <kurguz.vladimir@rezonans.ru>
2013-10-23 16:11:28 +0300

    Multiupload

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
 
   attr_accessible :title, :description, :price, :size, :care, :is_new, :make, :material, :images_attributes
 
-  has_many :images, :as => :attachable, :dependent => :destroy
+  has_many :images, :as => :attachable, :dependent => :destroy #http://lucapette.com/rails/multiple-files-upload-with-carrierwave-and-nested_form/
 
   accepts_nested_attributes_for :images, :allow_destroy => true, reject_if: :all_blank
 
